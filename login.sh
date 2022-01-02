e382f094e937ff1f4fc12b288d0181ac5f11bf1a
fernando.porcel <fernando.porcel@students.salle.url.edu>
2022-01-02 11:49:42 +0100

    Actualitzacio de confirmacio d'entrada al login

--- login.sh
+++ login.sh
@@ -5,24 +5,32 @@ echo
 read user
 read password
 
-if [[ $user = *"pi"* ]] && [[ $password = *"raspberry"* ]]; then
-    echo -e "<!DOCTYPE html PUBLIC\"-//W3C//DTD HTML 4.01 Strict//EN\"
+user=${user::-1}
+userOk=$(echo "$user" | cut -c 6-)
+password=${password::-1}
+passwordOk=$(echo "$password" | cut -c 10-)
+
+if [[ $userOk = "pi" ]] && [[ $passwordOk = "raspberry" ]]; then
+
+
+	echo -e "<!DOCTYPE html PUBLIC\"-//W3C//DTD HTML 4.01 Strict//EN\"
             \"http://www.w3.org/TR/html4/strict.dtd\">
+			<html>
 
-    <html>
-        <body>
-            <head>
-                <title>The sound of sunshine</title>
-            </head>
+			<head>
+			    <title>HTML Redirect</title>
+			    <meta http-equiv="refresh" content=\"5; url =
+			    http://raspberrypi/menu.sh\" />
+			</head>
 
-            <h3><font face="Helvetica" size="20px" color="\#FF7A59">Login</font></h3>
-            <h4><p style="color:green">L'usuari es: $user</p></h4>
-            <h4><p style="color:green">La contrasenya es upps: $password</p></h4>
+			<body>
 
+                <h4><p style="color:green">Login sucessful, you'll be redirected to the menu in 5 seconds.</p></h4>
 
-            </body>
-    </html>
-    "
+			</body>
+
+			</html>
+"
 else
     echo -e "<!DOCTYPE html PUBLIC\"-//W3C//DTD HTML 4.01 Strict//EN\"
             \"http://www.w3.org/TR/html4/strict.dtd\">
@@ -48,3 +56,4 @@ else
     </html>
     "
 fi
+
