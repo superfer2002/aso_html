9a6c12c1ec79c0a383499ee00dae97d30a9a03af
fernando.porcel <fernando.porcel@students.salle.url.edu>
2022-01-07 09:54:00 +0100

    Pmanagement amb visualitzacio mes comode i mata processos

--- pmanagement.sh
+++ pmanagement.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
+
+
 echo Content-Type: text/html
 echo
 
-process=$(ps aux)
+ps aux > tasks
+
+lines=$(cat tasks | wc -l )
 
 echo -e "<!DOCTYPE html PUBLIC\"-//W3C//DTD HTML 4.01 Strict//EN\"
         \"http://www.w3.org/TR/html4/strict.dtd\">
+
 <html>
     <head>
         <title>The sound of sunshine</title>
     </head>
+
     <body>
 
-        <h3><font face="Helvetica" size="20px" color="\#3B83BD">Process Management</font></h3>
-		<h4><p style="color:black">$process</p></h4>
-
+        <h3><font face="Helvetica" size="10px" color="\#3B83BD">Process Management</font></h3>
+		<form action=\"/kill.sh\" method=\"get\" ENCTYPE=\"text/plain\">
+          <label for=\"user\">Enter process PID:</label><br>
+          <input type=\"text\" id=\"task\" name=\"task\"><br>
+          <input type=\"submit\" value=\"KILL\">
+        </form>
+		"
+		i=0
+		while [ $i -le $lines ]
+		do
+			echo -e "$(sed -n "$i"p tasks)"
+			echo -e "<br>"
+			i=$(( $i + 1 ))
+		done
+		echo -e "<br>"
+		echo -e "<form action=\"/menu.sh\" method=\"post\" ENCTYPE=\"text/plain\">
+          <input type=\"submit\" value=\"Main Menu\">
+        </form>
     </body>
 </html>
 "
