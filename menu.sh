71c0bd3b13cd16f570c40d15fcba2b45fc977489
fernando.porcel <fernando.porcel@students.salle.url.edu>
2022-01-07 11:28:14 +0100

    Afegim al menu principal totes les opcions

--- menu.sh
+++ menu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+ip4=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
+
 echo Content-Type: text/html
 echo
 
@@ -6,21 +9,36 @@ echo -e "<!DOCTYPE html PUBLIC\"-//W3C//DTD HTML 4.01 Strict//EN\"
         \"http://www.w3.org/TR/html4/strict.dtd\">
 
 <html>
-    <body>
-        <head>
-            <title>The sound of sunshine</title>
-        </head>
+    <head>
+        <title>The sound of sunshine</title>
+		<link rel="icon" type="image/jpg" href="Pictures/16250633403413.jpg" />
+    </head>
 
-        <h3><font face="Helvetica" size="20px" color="\#FF7A59">Login</font></h3>
-        <h4><p style="color:green">L'usuari es: $userOk</p></h4>
-        <h4><p style="color:green">La contrasenya es: $passwordOk</p></h4>
+    <body>
+        <h3><font face="Helvetica" size="15px" color="\#FF7A59">The sound of sunshine's Menu</font></h3>
+
         <button onclick=\"processFunction()\">Process Management</button>
+        <button onclick=\"monitoringFunction()\">Monitoring</button>
+        <button onclick=\"offFunction()\">Turn Off</button>
+        <button onclick=\"restartFunction()\">Restart</button>
+        <button onclick=\"logsFunction()\">Logs Management</button>
+        <button onclick=\"usersFunction()\">Users Management</button>
+        <button onclick=\"packetFunction()\">Packet Filtering</button>
+        <button onclick=\"tasksFunction()\">Pre-programmed tasks Management</button>
+        <button onclick=\"musicalFunction()\">Musical Functionalities</button>
 
         <script>
-        function processFunction() {
-            location.replace(\"http://raspberrypi/pmanagement.sh\")
+        function offFunction() {
+            location.replace(\"http://$ip4/turnoff.sh\")
         }
+        function processFunction() {
+            location.replace(\"http://$ip4/pmanagement.sh\")
+        }
+		function restartFunction() {
+			location.replace(\"http://$ip4/restart.sh\")
+		}
         </script>
-        </body>
+    </body>
 </html>
 "
+
