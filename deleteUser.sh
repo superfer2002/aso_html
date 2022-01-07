4b180a3fbbb645dbf3b4cc4f9e1f1b5fb0a58733
superfer2002 <fporcel2002@gmail.com>
2022-01-07 17:25:44 +0100

    Arreglo de errores de deleteUser

--- deleteUser.sh
+++ deleteUser.sh
@@ -6,25 +6,26 @@ echo Content-Type: text/html
 echo
 
 read user
-
 user=${user::-1}
-userOk=$(echo "$user" | cut -c 6-)
-
+userOk=$(echo $user | awk -F= '{print $2}')
 $(sudo deluser $userOk)
+
+$(sudo rm -r /home/$userOk)
 
 echo -e "<!DOCTYPE html PUBLIC\"-//W3C//DTD HTML 4.01 Strict//EN\"
 		\"http://www.w3.org/TR/html4/strict.dtd\">
 <html>
 	<head>
-		<title>HTML Redirect</title>
+		<title>The sound of sunshine</title>
 		<meta http-equiv="refresh" content=\"2; url =
 		http://$ip4/menu.sh\" />
 	</head>
-		<body>
 
-			<h4><p style="color:green">Delete sucessful, you'll be redirected in 2 seconds.</p></h4>
+	<body>
 
-		</body>
+		<h4><p style="color:green">Delete successful, you'll be redirected in 2 seconds.</p></h4>
+
+	</body>
 
 </html>
 "
